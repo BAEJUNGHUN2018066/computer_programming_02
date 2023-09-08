c7e3f415ac6c852768b76ad3b5b2f3b9fddb68f5
BAEJUNGHUN2018066 <144421342+BAEJUNGHUN2018066@users.noreply.github.com>
2023-09-08 16:17:18 +0900

    테스트 파일을 추가했습니다
    Description

--- programming_02.py
+++ programming_02.py
@@ -1,1 +1,4 @@
-print('hello world')
+name = input("이름를 입력하세요")
+number_one = input("숫자를입력하세요")
+number_two = input("숫자를 입력하세요")
+print('안녕하세요.',name,'님','입력하신 두 숫자는' ,number_one,',',number_two,'입니다')
